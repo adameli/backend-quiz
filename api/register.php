3be7436ef44208a552c6b55202222a441b7d8385
Adam Eliasson <adam.eliasson.swe@gmail.com>
2023-03-29 12:47:42 +0200

    the register api somewhat finished

--- api/register.php
+++ api/register.php
@@ -2,24 +2,39 @@
 ini_set("display_errors", 1);
 require_once "functions.php";
 //* http code 201, save user in a json file, return a objekt with username & password & points.
+
+// Controlls so that we only allow the content-type: application/json
+$contentType = $_SERVER["CONTENT_TYPE"];
+if ($contentType != "application/json"){
+    $message = ["error" => "This content-type $contentType is not allowad it must be 'application/json'"];
+    sendJson($message, 415);
+}
+
 $filename = "users.json";
+
+// Get the post request and put the objekt in $data
 $data = json_decode(file_get_contents("php://input"), true);
 $username = $data["username"];
 $password = $data["password"];
 
+// Creates new user with the input values
 $newuser = [
     "username" => $username,
     "password" => $password,
     "points" => 0,
 ];
 
+// Controlls if the file "users.json" exists, if false we put the newuser in a json file and send the newuser back to the webb
 if(!file_exists($filename)){
-    $testUser = ["username" => "test", "password" => "test"];
-    $json = json_encode($testUser, JSON_PRETTY_PRINT);
+    $json = json_encode([$newuser], JSON_PRETTY_PRINT);
     file_put_contents($filename, $json);
+    sendJson($newuser, 201);
+}else {
+    $users = json_decode(file_get_contents($filename), true);
 }
 
-$users = json_decode(file_get_contents($filename), true);
+// Controlls if there is already a user with the same username as the requested newuser
+// This will stop users from creating acounts with an already existing username
 foreach($users as $user){
     if($user["username"] == $username){
         $message = ["message" => "Conflict (the username is already taken)"];
@@ -27,7 +42,9 @@ foreach($users as $user){
     }
 }
 
-json_encode($newuser, JSON_PRETTY_PRINT);
-file_put_contents($filename, $newuser);
+// If all goes well and the controlls are checked we put the new users in our file and send back the newusers values
+$users[] = $newuser;
+$jsonUsers = json_encode($users, JSON_PRETTY_PRINT);
+file_put_contents($filename, $jsonUsers);
 sendJson($newuser, 201);
 ?>
